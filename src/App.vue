4dca7ee1e4b0af0e2b350bce12b0e8a6edeb8bb4
benchik <wygndo@gmail.com>
2025-11-03 15:49:23 +0300

    add checking init data

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
 import { retrieveRawInitData } from '@tma.js/sdk-vue'
+import { onMounted, ref } from 'vue'
 
-console.log(retrieveRawInitData);
+const rawData = ref<string>('')
+
+onMounted(() => {
+  rawData.value = retrieveRawInitData() ?? ''
+})
 </script>
 
 <template>
   <h1>You did it!</h1>
-  <span v-if="retrieveRawInitData">
-    <p>{{ retrieveRawInitData}}</p>
+  <span v-if="rawData">
+    <p>{{ rawData }}</p>
   </span>
 </template>
 
